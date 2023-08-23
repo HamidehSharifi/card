1b7f07f8e4593fe45b566b8215d54782fce1e639
hsharifi <hsharifi@fardup.ir>
2023-08-23 11:29:33 +0330

    secend Commit

--- src/main/java/com/example/card/Email/persistance/Seller.java
+++ src/main/java/com/example/card/Email/persistance/Seller.java
@@ -1,5 +1,12 @@
 package com.example.card.Email.persistance;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "seller")
 public class Seller {
 
 
